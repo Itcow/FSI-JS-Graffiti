c0be558c77323bac9da08282a008506fd7edc7da
Itcow <cowcerit@alumni.vcu.edu>
2022-02-10 20:34:05 -0500

    all done

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+//radius dog images
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++)
+{
+    dogImages[i].style.borderRadius = '50px'
+    //flip image
+    dogImages[i].style.transform = "scaleX(-1)";
+}
+
+//align dog names left
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){ dogNames[i].style.textAlign= "left"}
+
+//make footer color
+let footer = document.querySelector(".footer")
+footer.style.color = "pink";
+
+
